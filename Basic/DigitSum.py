636524345ae15617f816fcd085e0566d0ffa0439
devamangupta <aman.gupta.rn06@gmail.com>
2026-04-26 16:48:12 +0530

    Python Program to Find Sum of Digits of a Number

---
+++ Basic/DigitSum.py
@@ -0,0 +1,10 @@
+# Python Program to Find Sum of Digits of a Number
+n = int(input("Enter a number: "))
+def DigitSum(n):
+    sum = 0
+    while n > 0:
+        last_digit = n % 10
+        sum = sum + last_digit
+        n = n // 10
+    return sum
+print(DigitSum(n))

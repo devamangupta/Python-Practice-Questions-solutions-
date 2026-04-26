18f6e5e6c8407625a94f48a02f5b6421f4dc35ec
devamangupta <aman.gupta.rn06@gmail.com>
2026-04-26 23:44:20 +0530

    binary number program by creating function

--- Basic/binary.py
+++ Basic/binary.py
@@ -1,8 +1,26 @@
 # Python Program to Print Binary Equivalent of an Integer using Recursion
 n = int(input("Enter a number to find it's binary number: "))
+"""
+normal pogram with using iterator like while loop 
 binary = ""
 while n > 0:
     reminder = str(n % 2)
     n = n // 2
     binary = str(reminder + binary)
 print(binary)
+"""
+"""
+In this time we try to create a functin name binary 
+def binary(n):
+    binary_value = ""
+    while n > 0:
+        reminder = str(n % 2)
+        n = n//2
+        binary_value = str(reminder + binary_value)
+    
+    return binary_value
+print(binary(n))
+        
+"""
+"""
+Now to try to do it by recusrion """

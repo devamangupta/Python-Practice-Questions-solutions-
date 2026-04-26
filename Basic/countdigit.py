bd8f867a704d20fb3d1de5f101dc6c708fb05022
devamangupta <aman.gupta.rn06@gmail.com>
2026-04-26 19:19:08 +0530

    we try this program with the help of recursion

--- Basic/countdigit.py
+++ Basic/countdigit.py
@@ -1,16 +1,32 @@
 # Python Program to Count the Number of Digits in a Number
 n = int(input("Enter a number: "))
 """
+In this we just use loop in a simple format
 digit = 0
 while n > 0:
     digit += 1
     n = n // 10
 print(digit)
 """
+
+"""
+In this program we create a method
 def digitcount(n):
     digit = 0 
     while n > 0:
         digit += 1
         n = n //10
     return digit
+print(digitcount(n))
+"""
+
+"""
+In this program we create a recursion function
+"""
+def digitcount(n):
+    if n == 0:
+        return 0
+    digit = 0 
+    digit += 1
+    return digit + digitcount(n//10)
 print(digitcount(n))

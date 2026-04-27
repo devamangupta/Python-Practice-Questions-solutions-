e4f59202376fa292a1614233f73e06a44e47904d
devamangupta <aman.gupta.rn06@gmail.com>
2026-04-27 13:16:10 +0530

    cm to feet and inches

---
+++ Basic/cm_to_feet.py
@@ -0,0 +1,6 @@
+# Python Program to Convert Centimeters to Feet and Inches
+cm = int(input("Enter a value you want to change to feet and inches: "))
+inches = 0.394
+feet = 0.038
+print(f"{cm} in inches is {cm * inches}")
+print(f"{cm} in feet is {cm * feet}")

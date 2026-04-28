a7fa4b2de6d200200232adf4bbfe7c91255fb15b
devamangupta <aman.gupta.rn06@gmail.com>
2026-04-29 00:47:46 +0530

    check perfect number

--- Numbers/perfect_no.py
+++ Numbers/perfect_no.py
@@ -14,8 +14,7 @@ else:
     print("It's not a perfect Number...")
 """
 """
-In this we try to solve this problem with the help of function.."""
-
+In this we try to solve this problem with the help of function..
 def perfect_no(num):
     sum = 0
     for i in range(1, num):
@@ -26,3 +25,4 @@ def perfect_no(num):
     else:
         return f"{num} is not a perfect number!!!"
 print(perfect_no(n))
+"""

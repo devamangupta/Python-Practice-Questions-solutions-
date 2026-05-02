765dedbdf8bf26938f1c2ed73249f37a173e79d1
devamangupta <aman.gupta.rn06@gmail.com>
2026-05-02 16:28:05 +0530

    correction

--- Numbers/prime.py
+++ Numbers/prime.py
@@ -4,7 +4,7 @@ n = int(input("Enter a number to check it's prime or not: "))
 if n == 0:
     print(f"{n} is 1 so it's not a prime number...")
 for i in range(2,n):
-    if i % n == 0:
+    if n % i == 0:
         print(f"{n} is not a prime number...")
     else:
         print(f"{n} is a prime number...")

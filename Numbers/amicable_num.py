819e72fd354a5fe0f6f1ab1b6ec23d2fbde81bd6
devamangupta <aman.gupta.rn06@gmail.com>
2026-05-02 18:49:57 +0530

    Amicabl numbers

---
+++ Numbers/amicable_num.py
@@ -0,0 +1,15 @@
+# Python Program to Check If Two Numbers are Amicable Numbers or Not
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+sum1 = 0
+sum2 = 0
+for i in range(1, num1):
+    if num1 % i == 0:
+        sum1 += i
+for i in range(1, num2):
+    if num2 % i == 0:
+        sum2 += i
+if sum1 == num2 and sum2 == num1:
+    print(f"{num1} and {num2} are amicable number...")
+else:
+    print(f"{num1} and {num2} are not amicable number...")

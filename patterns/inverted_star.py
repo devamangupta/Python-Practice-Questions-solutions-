f1475f44039942f467e65a8a1279325e59cc7681
devamangupta <aman.gupta.rn06@gmail.com>
2026-05-03 23:09:26 +0530

    another way to print inverted star pattern

--- patterns/inverted_star.py
+++ patterns/inverted_star.py
@@ -1,6 +1,12 @@
 # WAP to print Inverted star pattern 
+"""
 n = int(input("Enter a number: "))
 for i in range(n,0,-1):
     for j in range(0,i):
         print("*", end= "")
+    print()
+"""
+rows = int(input("Enter a number: "))
+for i in range(rows,0,-1):
+    print("*" * i, end = " ")
     print()

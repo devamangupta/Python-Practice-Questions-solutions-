1576fd136ad42f07010b6c62c2555132742a73f1
devamangupta <aman.gupta.rn06@gmail.com>
2026-05-05 12:04:21 +0530

    Calculate quotient and remainder

---
+++ Math/quotient_remainder.py
@@ -0,0 +1,7 @@
+# Python Program to Find Quotient and Remainder of Two Numbers
+n1 = int(input("Enter a number: "))
+n2 = int(input("Enter another number: "))
+r = n1 % n2
+q = n1 // n2
+print(f"Remainder of {n1} and {n2} is {r}")
+print(f"Quotient of {n1} and {n2} is {q}")

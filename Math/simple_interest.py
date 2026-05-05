7d892a65efb529bd1fdf09cc57ee4250999a499e
devamangupta <aman.gupta.rn06@gmail.com>
2026-05-05 11:59:31 +0530

    Calculate simple intrust

---
+++ Math/simple_interest.py
@@ -0,0 +1,6 @@
+# WAP to find simple interest
+p = int(input("Enter amount: "))
+r = int(input("Enter rate of intrust: "))
+t = int(input("Enter time period in year: "))
+si = (p*r*t)/100
+print(f"Simple Interest of {p} amount is {si}")
